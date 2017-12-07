3da574a35a6b024081da7e89d4bd1ebd534763d8
wangez <1059601437@qq.com>
2017-12-07 17:44:00 +0800

    clear

--- src/loges/index.js
+++
@@ -1,9 +0,0 @@
-const logs = require.context('.', false, /\.js$/)
-const Logs = {}
-
-logs.keys().forEach(key => {
-  if (key === './index.js') return
-  Logs[key.replace(/(\.\/|\.js)/g, '').toLowerCase()] = logs(key).default
-})
-
-export default Logs
